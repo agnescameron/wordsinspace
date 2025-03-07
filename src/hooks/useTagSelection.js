6267635d0712c28e923a6093ebf702064725e575
agnescameron <agnesfcameron@gmail.com>
2025-03-07 19:01:19 +0000

    cleaning up

--- src/hooks/useTagSelection.js
+++ src/hooks/useTagSelection.js
@@ -69,39 +69,18 @@ export const useTagSelection = (tags, isTagMode) => {
 
   // extract slugs from the tags that are set to 'checked: true'
   const slugs = tags.map(d => d.slug)
-  // const taxArr = slugs.length > 0 ? slugs.map( slug => ({
-  //   taxonomy: "TAG",
-  //   field: "SLUG",
-  //   terms: [slug],
-  //   operator: "IN"
-  // })) : null;
 
-// const taxQuery = {
-//   taxArray: [
-//     { field: 'SLUG', terms: ["libraries"], taxonomy: 'TAG' },
-//     { field: 'SLUG', terms: ["archives"], taxonomy: 'TAG' }
-//   ],
-//   relation: 'AND'
-// };
-
-// const taxArray = [
-//   { field: "SLUG", terms: ["libraries"], taxonomy: "TAG" },
-//   { field: "SLUG", terms: ["archives"], taxonomy: "TAG" }
-// ];
-
+  // use wordpress taxonomy array
   const taxArray = slugs.map(slug => ({
       taxonomy: "TAG",
       field: "SLUG",
       terms: [slug]  // Must be an array
     }))
 
-  console.log('taxarr', taxArray)
-
   // GraphQL query to get the list of Posts and Pages of a selected Tag
   const response = useQuery(SEARCH_TAGS_QUERY, {
     variables: {first: 100, taxArray: taxArray},
     skip: !isTagMode || !taxArray
   })
-  console.log(response)
   return response
 }
