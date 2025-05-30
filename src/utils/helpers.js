d492ed57061ad8947e69836ecd0f40620db68b1d
agnescameron <agnesfcameron@gmail.com>
2025-05-30 19:12:50 +0100

    give logging a rest

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -96,7 +96,7 @@ const getCoOccurringTags = (tags, filterTags, catName) => {
     nodes?.forEach(item => {
       const matchesCategory = catName === "" || item.categories?.nodes[0]?.name.toLowerCase() === catName;
       const hasFilterTags = filterTags.every(t => item.tags?.nodes?.map(n => n.slug?.toLowerCase()).includes(t.slug.toLowerCase()))//item.tags?.nodes?.map(n => n.slug?.toLowerCase()).includes(t.slug.toLowerCase()));
-      console.log(item.tags?.nodes?.map(n => n.slug?.toLowerCase()), filterTags, hasFilterTags, matchesCategory, catName, item.categories?.nodes[0]?.name.toLowerCase())
+      // console.log(item.tags?.nodes?.map(n => n.slug?.toLowerCase()), filterTags, hasFilterTags, matchesCategory, catName, item.categories?.nodes[0]?.name.toLowerCase())
 
       if (matchesCategory && hasFilterTags) {
         item.tags?.nodes?.forEach(node => {
@@ -111,7 +111,7 @@ const getCoOccurringTags = (tags, filterTags, catName) => {
 
   let intersection = new Set(coTagSlugs);
 
-  console.log(Array.from(intersection))
+  // console.log(Array.from(intersection))
   return Array.from(intersection);
 };
 
